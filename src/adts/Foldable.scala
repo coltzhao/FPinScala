428b194781470906d70aef6c662df66186ee4e56
Hang Zhao <hzhao@kabam.com>
2014-01-04 22:25:40 -0800

    add third batch of adts

--- src/adts/Foldable.scala
+++ src/adts/Foldable.scala
@@ -7,14 +7,14 @@ trait Monoid[A] extends Semigroup[A] {
 }
 
 trait Foldable[F[_]]  {
-    def foldMap[A, M: Monoid](t: F[A], f: A => M): M
+  def foldMap[A, M: Monoid](t: F[A], f: A => M): M
 
-    /*
-      def fold[M: Monoid](t: F[M]): M // also called reduce with variance
-        def foldRight[A, B](t: F[A], z: => B, f: (A, B) => B): B 
-          def foldLeft[A, B](t: F[A], z: B, f: (B, A) => B): B
-            def foldr1[A, B](t: F[A], f: (A, => A) => A): Option[A]
-              def foldl1[A, B](t: F[A], f: (A, A) => A): Option[A]
-              */
+/*
+  def fold[M: Monoid](t: F[M]): M // also called reduce with variance
+  def foldRight[A, B](t: F[A], z: => B, f: (A, B) => B): B 
+  def foldLeft[A, B](t: F[A], z: B, f: (B, A) => B): B
+  def foldr1[A, B](t: F[A], f: (A, => A) => A): Option[A]
+  def foldl1[A, B](t: F[A], f: (A, A) => A): Option[A]
+*/
 }
 
